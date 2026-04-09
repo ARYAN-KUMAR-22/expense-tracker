4df319cd0e870fd3c3481918841e0c791f59b1c7
Aryan Kumar <aryan@example.com>
2026-04-09 14:36:16 +0530

    Undeploy from GitHub Pages, revert Vite configurations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
